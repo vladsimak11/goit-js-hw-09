0a6b0d73ad5192dc3eb7d3171aee3af5c00d927f
Vladyslav Simak <simakv21@gmail.com>
2023-02-13 12:26:53 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timerId = null;
+
+start.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  start.disabled = true;
+  stop.disabled = false;
+});
+
+stop.addEventListener('click', () => {
+  clearInterval(timerId);
+  start.disabled = false;
+  stop.disabled = true;
+});
